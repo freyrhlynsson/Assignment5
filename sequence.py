3264a5f9f670c8a5a4113ccc6208d2a04e70818c
Freyr Hlynsson <freyr19@ru.is>
2020-09-03 10:01:27 +0000

    Ädded comments for clarity

--- sequence.py
+++ sequence.py
@@ -4,19 +4,26 @@
 # we stop when we have the n numbers requested
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
 
-n_1,n_2,n_3 = 1,2,3
-
 if n < 4:
     n_1 = 0
+    
+    # Since first three are 1,2,3 we simply 
+    # add 1 to number starting from 0, n times
+    # and print out each time
     for i in range(0,n):
         n_1 += 1
         print(n_1)
 else:
+    # Initialize first three to compute larger values
+    n_1,n_2,n_3 = 1,2,3
+
     for number in (n_1,n_2,n_3):
         print(number)
     for number in range(0,n-3):
         value = n_1 + n_2 + n_3
         print(value)
+
+        # Shiting values for next iteration
         n_1 = n_2
         n_2 = n_3
         n_3 = value
