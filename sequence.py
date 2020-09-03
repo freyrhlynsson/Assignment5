3be717b39c317a8f4523a6e586d184efab41ff13
Freyr Hlynsson <freyr19@ru.is>
2020-09-03 09:57:01 +0000

    Inputs larger than 3 coded

--- sequence.py
+++ sequence.py
@@ -2,3 +2,19 @@
 # Check if user input is larger than 3,
 # If it is we add three numbers together, get the new one and shift the variables up the sequence
 # we stop when we have the n numbers requested
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+n_1,n_2,n_3 = 1,2,3
+
+if n < 4:
+    a = 0
+    print(a)
+else:
+    for number in (n_1,n_2,n_3):
+        print(number)
+    for number in range(0,n-3):
+        value = n_1 + n_2 + n_3
+        print(value)
+        n_1 = n_2
+        n_2 = n_3
+        n_3 = value
