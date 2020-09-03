035ca4735bf6fb2d2957b3b04d76cd88f7a76ac8
Freyr Hlynsson <freyr19@ru.is>
2020-09-03 09:47:39 +0000

    Writing algorithm of sequence

---
+++ sequence.py
@@ -0,0 +1,4 @@
+# Initialize first three values of the sequence
+# Check if user input is larger than 3,
+# If it is we add three numbers together, get the new one and shift the variables up the sequence
+# we stop when we have the n numbers requested
